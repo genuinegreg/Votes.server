b9bd3b7dcf047e2594fab0d4db47c25a1759cc34
Grégoire Audoux <projects@gregoire-audoux.fr>
2014-10-14 00:11:18 +0200

    Initial code comit

---
+++ api/controllers/VoteController.js
@@ -0,0 +1,11 @@
+/**
+ * VoteController
+ *
+ * @description :: Server-side logic for managing Votes
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/VoteOptionController.js
@@ -0,0 +1,11 @@
+/**
+ * VoteOptionController
+ *
+ * @description :: Server-side logic for managing Voteoptions
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
